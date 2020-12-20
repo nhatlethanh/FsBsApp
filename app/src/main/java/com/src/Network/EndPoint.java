b1c4fabdc8974170284973138d5c236b2922292a
Lê Thành Nhật <69852815+nhatlethanh@users.noreply.github.com>
2020-12-20 10:45:24 +0700

    Update

--- app/src/main/java/com/src/Network/EndPoint.java
+++ app/src/main/java/com/src/Network/EndPoint.java
@@ -2,7 +2,7 @@ package com.src.Network;
 
 public class EndPoint {
     static String BASE_URL = "http://192.168.0.115:5000/api/";
-    public static String BASE_URL_PUBLIC = "http://192.168.0.115:5000/public/photo/";
+    public static String BASE_URL_PUBLIC = "http://192.168.0.1155000/public/photo/";
 
     //**** User ****//
     static
